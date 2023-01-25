3686387b6c4588d1779b9a9c5d804bd83fae7c36
YASSINero <afriadeamassine@gmail.com>
2023-01-25 02:39:30 +0100

    templated the queue container optimised merge loop

--- DSA_practice.cpp
+++ DSA_practice.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <vector>
+
 #include "Queue.h"
 
 int main()
 {
-    Queue q(15), q2(20);
-
+    Queue<int> q(15), q2(20);
 
     q.push(18);
     q2.push(19);
@@ -20,7 +21,7 @@ int main()
     
     q = q + q2;
 
-    Queue q3(q);
+    Queue<int> q3(q);
     
 
 }
--- Queue.h
+++ Queue.h
@@ -3,23 +3,79 @@
 #include <algorithm>
 #include <memory>
 
-
+template<typename T>
 class Queue
 {
+	int frontInd, backInd;
+	T *queue;
+	size_t size_, count_;
+
+
 public:
 	Queue() = default;
-	Queue(size_t);
+	Queue(size_t q_size): frontInd(0), backInd(-1), queue(nullptr), size_(q_size), count_(0)
+	{
+		if (q_size > 0)
+			queue = new T[q_size];
 
-	~Queue();
-	Queue(const Queue& other);
+		std::fill_n(queue, static_cast<int>(q_size), -1);
+
+	}
+
+	~Queue() { delete[] queue; }
+
+	Queue(const Queue& other) : frontInd(other.frontInd), backInd(other.backInd), queue(nullptr), size_(other.size_), count_(other.count_)
+	{
+		if (size_ > 0)
+		{
+			queue = new T[size_];
+			std::memcpy(queue, other.queue, sizeof(T) * size_);
+		}
+	}
 
 public:
-	bool push(int);
-	bool pop();
-	bool isEmpty() const;
-	int	 front() const;
-	size_t size() const;
-	size_t count() const;
+	bool push(T val)
+	{
+		if (backInd < static_cast<int>(size_))
+		{
+			*(queue + (++backInd)) = val;
+			count_++;
+			return queue[backInd] == val;
+		}
+		return false;
+	}
+
+	bool pop()
+	{
+		if (!isEmpty())
+		{
+			queue[frontInd++] = 0;
+			count_--;
+			return true;
+		}
+		return false;
+	}
+
+	bool isEmpty() const
+	{
+		return frontInd > backInd;
+	}
+
+	auto front() const
+	{
+		return queue[frontInd];
+	}
+
+	size_t size() const
+	{
+		return size_;
+	}
+
+	size_t count() const
+	{
+		return count_;
+	}
+
 
 public:
 
@@ -30,50 +86,7 @@ public:
 		return *this;
 	}
 
-
-private:
-	int frontInd, backInd;
-	int *queue;
-
-	size_t size_, count_;
-
-private:
-	static void qMerge(const Queue& q1, const Queue& q2, Queue& dest_q)
-	{
-		//int* qTemp = new int[q1.size_ + q2.size_];
-
-		auto uniqTemp = std::make_unique<int[]>(q1.size_ + q2.size_);
-
-		//or Todo swap pointers / ranges
-		//std::swap(*q1.queue, *q2.queue);
-		std::swap_ranges(q1.queue, q1.queue + q1.size_, uniqTemp.get());
-
-		//then copy from q2
-		for(size_t i = 0; i < q1.size_; i++)
-		{
-			if(i > 0 ? uniqTemp.get()[i] == -1 : false)
-			{
-				for (size_t j = 0; j < q2.size_; j++)
-				{
-
-					if (q2.queue[j])
-					{
-						std::swap_ranges(q2.queue + j, q2.queue + q2.size_, uniqTemp.get() + i);
-						break;
-					}
-				}
-				break;
-			}
-		}
-
-		dest_q.queue = uniqTemp.release();	//data in this line is as wanted
-
-/*Todo:				However it looks like the memory gets freed bc of uniqPtr*/
-	}
-
-	friend void swap(Queue& ,Queue&) noexcept;
-
-	friend Queue operator+(Queue left_q, const Queue &other_q)
+	friend Queue operator+(Queue<T> left_q, const Queue<T> &other_q)
 	{
 		//Todo Provide commented explanation & perfect logic 
 		if(left_q.isEmpty() || other_q.isEmpty())
@@ -81,11 +94,10 @@ private:
 			return other_q.isEmpty() ? left_q : other_q;
 		}
 
-		Queue tempQ(left_q.size_ + other_q.size_);
+		Queue<T> tempQ(left_q.size_ + other_q.size_);
 
 		tempQ.count_ = left_q.count_ + other_q.count_;
-		
-		//Todo qMerge is imperfect
+
 		qMerge(left_q, other_q, tempQ);
 
 		for(size_t i = 0; i < tempQ.size_; i++)
@@ -102,4 +114,43 @@ private:
 
 		return tempQ;
 	}
+
+
+private:
+		//Todo Perfect loop, see online gdb for ideas
+	static void qMerge(const Queue& q1, const Queue& q2, Queue& dest_q)
+	{
+		auto uniqTemp = std::make_unique<T[]>(q1.size_ + q2.size_);
+		std::fill(uniqTemp.get(), uniqTemp.get() + q1.size_ + q2.size_, -1);
+
+		std::swap_ranges(q1.queue, q1.queue + q1.size_, uniqTemp.get());
+
+		int* range1 = uniqTemp.get()+1, * range2 = q2.queue;
+
+		//then copy from q2
+		//Todo test simplified loop
+		while (*range2 == (*range1 != -1 ? *range2 : 0))
+		{
+			range1 = *range1 != -1 ? range1 + 1 : range1;
+			range2 = *range2 == 0 ? range2 + 1 : range2;
+		}
+
+		std::memcpy(range1, range2, q2.size_);
+
+		dest_q.queue = uniqTemp.release();	//data in this line is as wanted
+
+	}
+
+	friend void swap(Queue<T>& q1, Queue<T>& q2) noexcept
+	{
+		using std::swap;
+
+		//swap all 
+		swap(q1.frontInd, q2.frontInd);
+		swap(q1.backInd, q2.backInd);
+		swap(q1.queue, q2.queue);
+		swap(q1.size_, q2.size_);
+		swap(q1.count_, q2.count_);
+	}
+
 };
